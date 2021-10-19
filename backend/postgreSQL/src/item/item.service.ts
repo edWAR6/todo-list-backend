2f005517e70f06039ae54f44c2637136a9b3d1d1
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2021-10-19 08:39:56 -0600

    fixed bug

--- backend/postgreSQL/src/item/item.service.ts
+++ backend/postgreSQL/src/item/item.service.ts
@@ -5,9 +5,9 @@ class PostgreSQLItemService implements ItemService {
 
   constructor(private database: PoolClient){}
 
-  async createItem(item: IItem): Promise<IItem> {
-    const query = `INSERT INTO item (list_id, description, checked) VALUES ($1, $2, $3)`;
-    const result = await this.database.query(query, [item.listId, item.description, item.checked]);
+  async createItem(listId: ID, item: IItem): Promise<IItem> {
+    const query = `INSERT INTO item (list_id, description, checked) VALUES ($1, $2, $3) RETURNING _id as id, list_id as listId, description, checked`;
+    const result = await this.database.query(query, [listId, item.description, item.checked]);
     return result.rows[0] as IItem;
   }
 
@@ -30,13 +30,13 @@ class PostgreSQLItemService implements ItemService {
   }
 
   async changeItemDescription(listId: ID, id: ID, description: string): Promise<IItem> {
-    const query = `UPDATE item SET description = $1 WHERE list_id = $2 AND _id = $3`;
+    const query = `UPDATE item SET description = $1 WHERE list_id = $2 AND _id = $3 RETURNING _id as id, list_id as listId, description, checked`;
     const result = await this.database.query(query, [description, listId, id]);
     return result.rows[0] as IItem;
   }
 
   async changeItemChecked(listId: ID, id: ID, checked: boolean): Promise<IItem> {
-    const query = `UPDATE item SET checked = $1 WHERE list_id = $2 AND _id = $3`;
+    const query = `UPDATE item SET checked = $1 WHERE list_id = $2 AND _id = $3 RETURNING _id as id, list_id as listId, description, checked`;
     const result = await this.database.query(query, [checked, listId, id]);
     return result.rows[0] as IItem;
   }
